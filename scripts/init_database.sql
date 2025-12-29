1ec9053ca934430776047ef5c7fb0a33275da903
Shashi Kunigiri <shashikunigiri1@gmail.com>
2025-12-29 17:03:16 +0530

    Layers Creation script created

---
+++ scripts/init_database.sql
@@ -0,0 +1,24 @@
+/*
+===============================================================================
+ Script Name : init_database.sql
+ Purpose     : Initialises MySQL Data Warehouse using Medallion Architecture.
+               Creates databases for bronze, silver and gold layers.
+ Author      : Newbie
+ Created On  : 2025-12-29
+
+ WARNING:
+ - This script is destructive if databases already exist
+   It drops all warehouse databases before recreating them
+ - Risk of permanent data loss
+===============================================================================
+*/
+
+-- drop existing databases
+DROP DATABASE IF EXISTS bronze;
+DROP DATABASE IF EXISTS silver;
+DROP DATABASE IF EXISTS gold;
+
+-- Create Medallion layer databases
+CREATE DATABASE bronze;
+CREATE DATABASE silver;
+CREATE DATABASE gold;
